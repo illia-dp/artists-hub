8a42680ae38a6398d2ca920c30033904fcceb21d
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-05-16 18:12:22 -0600

    add icons to the buttons

--- src/js/artists/create-markup-artists.js
+++ src/js/artists/create-markup-artists.js
@@ -28,12 +28,11 @@ export function createArtistsMarkup(arr) {
             <div class="artists-content">
               <h4 class="artists-name">${strArtist}</h4>
               <p class="artists-descr">${strBiographyEN}</p>
-              <button
-                type="button"
-                class="artists-learn-more-btn"
-                data-id="${_id}"
-              >
+              <button type="button" class="artists-learn-more-btn" data-id="${_id}">
                 Learn More
+                  <svg class="icon-caret-right" width="24" height="24">
+                    <use href="../img/sprite.svg#icon-caret-right"></use>
+                  </svg>
               </button>
             </div>
           </li>
