e2abde26248fb6bee3aa4737c897f6ff03ca33e5
ivannakm <ivadatalover@gmail.com>
2025-05-18 19:28:18 +0200

    fix

--- src/js/hero/hero.js
+++
@@ -1,7 +0,0 @@
-  document.querySelector('.hero-btn-link').addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
